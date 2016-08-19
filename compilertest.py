3ed5db1bf8ebbda17a4a701b643be1c3b8772608
Jessy Diamond Exum <jessy.diamondman@gmail.com>
2016-08-19 12:56:17 -0700

    Moved primitive compilation code from compilertest.py to the command_queue class

--- compilertest.py
+++ compilertest.py
@@ -5,17 +5,8 @@ import time
 from bitarray import bitarray
 from flask import Flask, escape, render_template
 
-from pprint import pprint
-
-import proteusisc
 from proteusisc.controllerManager import _controllerfilter
 from proteusisc.jtagScanChain import JTAGScanChain
-from proteusisc.jtagStateMachine import JTAGStateMachine
-from proteusisc.frame import FrameSequence
-from proteusisc.jtagDevice import JTAGDevice
-from proteusisc import errors as proteusiscerrors
-from proteusisc.primitive import DeviceTarget, Level3Primitive,\
-    Level2Primitive, ExpandRequiresTAP, Executable
 from proteusisc.test_utils import FakeDev
 
 #drvr = _controllerfilter[0x03FD][0x0008]
@@ -52,227 +43,16 @@ chain.sleep(delay=1)
 chain.sleep(delay=2)
 chain.sleep(delay=1)
 
-def mergePrims(inchain):
-    if isinstance(inchain, FrameSequence):
-        merged_prims = FrameSequence(chain)
-    else:
-        merged_prims = []
-    working_prim = inchain[0]
-    i = 1
-    while i < len(inchain):
-        tmp = inchain[i]
-        res = working_prim.merge(tmp)
-        if res is not None:
-            working_prim = res
-        else:
-            merged_prims.append(working_prim)
-            working_prim = tmp
-        i += 1
-    merged_prims.append(working_prim)
-    return merged_prims
-
-
 app = Flask(__name__)
 
 @app.route('/')
 def report():
-    if len(chain._command_queue) == 0:
-        return "No commands in Queue."
     t = time.time()
     stages = []
     stagenames = []
 
-    ######################### STAGE 01 #########################
-    ###################### INITIAL PRIMS! ######################
-
-    stages.append([chain.snapshot_queue()])
-    stagenames.append("Input Stream")
-
-    ######################### STAGE 02 #########################
-    ############### GROUPING BY EXEC BOUNDARIES!################
-
-    fences = []
-    fence = [chain._command_queue[0]]
-    for p in chain._command_queue[1:]:
-        if type(fence[0])._layer == type(p)._layer and\
-           isinstance(fence[0], DeviceTarget) == \
-              isinstance(p, DeviceTarget):
-            fence.append(p)
-        else:
-            fences.append(fence)
-            fence = [p]
-    fences.append(fence)
-
-    formatted_fences = []
-    for fence in fences:
-        formatted_fence = [p.snapshot() for p in fence]
-        formatted_fences.append(formatted_fence)
-        formatted_fences.append([])
-    stages.append(formatted_fences[:-1]) #Ignore trailing []
-    stagenames.append("Fencing off execution boundaries")
-
-    ######################### STAGE 03 #########################
-    ############## SPLIT GROUPS BY DEVICE TARGET! ##############
-
-    split_fences = []
-    for fence in fences:
-        tmp_chains = {}
-        for p in fence:
-            k = p._device_index \
-                if isinstance(p, DeviceTarget) else "chain"
-            subchain = tmp_chains.setdefault(k, []).append(p)
-        split_fences.append(list(tmp_chains.values()))
-
-    formatted_split_fences = []
-    for fence in split_fences:
-        for group in fence:
-            formatted_split_fences.append([p.snapshot() for p in group])
-        formatted_split_fences.append([])
-    stages.append(formatted_split_fences[:-1])
-    stagenames.append("Grouping prims of each boundary by target device")
-
-    ######################### STAGE 04 #########################
-    ############## ALIGN SEQUENCES AND PAD FRAMES ##############
-
-    grouped_fences = [
-        FrameSequence(chain, *fence).finalize()
-        for f_i, fence in enumerate(split_fences)
-    ]
-
-    formatted_grouped_fences = []
-    for fence in grouped_fences:
-        formatted_grouped_fences += fence.snapshot() + [[]]
-    stages.append(formatted_grouped_fences[:-1])
-    stagenames.append("Aligning and combining each group dev prim stream")
-
-    ######################### STAGE 05 #########################
-    ################## RECOMBINE FRAME GROUPS ##################
-
-    ingested_chain = grouped_fences[0]
-    for fence in grouped_fences[1:]:
-        ingested_chain += fence
-
-    stages.append(ingested_chain.snapshot())
-    stagenames.append("Recombining sanitized execution boundaries")
-
-    ###################### POST INGESTION ######################
-    ######################### STAGE 6+ #########################
-    ################ Flatten out LV3 Primitives ################
-    while(any((f._layer == 3 for f in ingested_chain))):
-        ################# COMBINE COMPATIBLE PRIMS #################
-        ingested_chain = mergePrims(ingested_chain)
-
-        stages.append(ingested_chain.snapshot())
-        stagenames.append("Combining compatible lv3 prims.")
-
-        ################ TRANSLATION TO LOWER LAYER ################
-        sm = JTAGStateMachine(chain._sm.state)
-        expanded_prims = FrameSequence(chain)
-        for f in ingested_chain:
-            if f._layer == 3:
-                expanded_prims += f.expand_macro(sm)
-            else:
-                expanded_prims.append(f)
-        expanded_prims.finalize()
-        ingested_chain = expanded_prims
-
-        stages.append(ingested_chain.snapshot())
-        stagenames.append("Expanding lv3 prims")
-
-
-    ######################### STAGE 8+ #########################
-    ############## Flatten out Dev LV2 Primitives ##############
-    while(any((isinstance(f._valid_prim, DeviceTarget)
-               for f in ingested_chain))):
-        ################# COMBINE COMPATIBLE PRIMS #################
-
-        ingested_chain = mergePrims(ingested_chain)
-
-        stages.append(ingested_chain.snapshot())
-        stagenames.append("Merging Device Specific Prims")
-
-        ################ TRANSLATION TO LOWER LAYER ################
-
-        sm = JTAGStateMachine(chain._sm.state)
-        expanded_prims = FrameSequence(chain)
-        for f in ingested_chain:
-            if issubclass(f._prim_type, DeviceTarget):
-                expanded_prims += f.expand_macro(sm)
-            else:
-                expanded_prims.append(f)
-        expanded_prims.finalize()
-        ingested_chain = expanded_prims
-
-        stages.append(ingested_chain.snapshot())
-        stagenames.append("Expanding Device Specific Prims")
-
-    ############ Convert FrameSequence to flat array ###########
-    flattened_prims = [f._valid_prim for f in ingested_chain]
-    stages.append([[p.snapshot() for p in flattened_prims]])
-    stagenames.append("Converting format to single stream.")
-
-    del ingested_chain
-
-    ####################### STAGE 10+ #########################
-    ######### Flatten out remaining macros Primitives #########
-    while (not all((isinstance(p, (ExpandRequiresTAP,Executable))
-                    for p in flattened_prims))):
-        ################# COMBINE COMPATIBLE PRIMS #################
-        flattened_prims = mergePrims(flattened_prims)
-
-        stages.append([[p.snapshot() for p in flattened_prims]])
-        stagenames.append("Merging Device Agnostic LV2 Prims")
-
-        ################ TRANSLATION TO LOWER LAYER ################
-        sm = JTAGStateMachine(chain._sm.state)
-        expanded_prims = []
-        for p in flattened_prims:
-            tmp = p.expand(chain, sm) if not \
-                  isinstance(p, ExpandRequiresTAP) else None
-            if tmp:
-                expanded_prims += tmp
-            else:
-                expanded_prims.append(p)
-        flattened_prims = expanded_prims
-
-        stages.append([[p.snapshot() for p in flattened_prims]])
-        stagenames.append("Expanding Device Agnostic LV2 Prims")
-
-
-    ################# COMBINE COMPATIBLE PRIMS #################
-    flattened_prims = mergePrims(flattened_prims)
-
-    stages.append([[p.snapshot() for p in flattened_prims]])
-    stagenames.append("Final LV2 merge")
-
-    ################### EXPAND TO LV1 PRIMS ####################
-    sm = JTAGStateMachine(chain._sm.state)
-    expanded_prims = []
-    for p in flattened_prims:
-        tmp = p.expand(chain, sm)
-        if tmp:
-            expanded_prims += tmp
-        else:
-            expanded_prims.append(p)
-    flattened_prims = expanded_prims
-
-    stages.append([[p.snapshot() for p in flattened_prims]])
-    stagenames.append("Expand to LV1 Primitives")
-
-
-    if not all((isinstance(p, Executable) for p in flattened_prims)):
-        raise proteusiscerrors.ProteusISCException(
-            "Reduction did not produce executable instruction sequence.")
-
-
-    #################### COMBINE LV1 PRIMS #####################
-
-    flattened_prims = mergePrims(flattened_prims)
-
-    stages.append([[p.snapshot() for p in flattened_prims]])
-    stagenames.append("Final LV2 merge")
-
-    ############################################################
+    chain._command_queue._compile(debug=True, stages=stages,
+                                  stagenames=stagenames)
 
     print(time.time()-t)
 
--- proteusisc/command_queue.py
+++ proteusisc/command_queue.py
@@ -2,6 +2,12 @@ import collections
 
 from .jtagStateMachine import JTAGStateMachine
 
+from .jtagStateMachine import JTAGStateMachine
+from .frame import FrameSequence
+from .errors import ProteusISCError
+from .primitive import DeviceTarget, ExpandRequiresTAP, Executable
+
+
 class CommandQueue(collections.MutableSequence):
     def __init__(self, chain):
         self.queue = []
@@ -33,3 +39,232 @@ class CommandQueue(collections.MutableSequence):
     def append(self, elem):
         elem._chain = self._chain
         super(CommandQueue, self).append(elem)
+
+    def _compile(self, debug=False, stages=None, stagenames=None,
+                 dryrun=False):
+        if len(self) == 0:
+            return "No commands in Queue."
+
+        ###################### INITIAL PRIMS! ######################
+
+        if debug:
+            stages.append([self._chain.snapshot_queue()])
+            stagenames.append("Input Stream")
+
+        ############### GROUPING BY EXEC BOUNDARIES!################
+
+        fences = []
+        fence = [self[0]]
+        for p in self[1:]:
+            if type(fence[0])._layer == type(p)._layer and\
+               isinstance(fence[0], DeviceTarget) == \
+                  isinstance(p, DeviceTarget):
+                fence.append(p)
+            else:
+                fences.append(fence)
+                fence = [p]
+        fences.append(fence)
+
+        if debug:
+            formatted_fences = []
+            for fence in fences:
+                formatted_fence = [p.snapshot() for p in fence]
+                formatted_fences.append(formatted_fence)
+                formatted_fences.append([])
+            stages.append(formatted_fences[:-1]) #Ignore trailing []
+            stagenames.append("Fencing off execution boundaries")
+
+        ############## SPLIT GROUPS BY DEVICE TARGET! ##############
+
+        split_fences = []
+        for fence in fences:
+            tmp_chains = {}
+            for p in fence:
+                k = p._device_index \
+                    if isinstance(p, DeviceTarget) else "chain"
+                subchain = tmp_chains.setdefault(k, []).append(p)
+            split_fences.append(list(tmp_chains.values()))
+
+        if debug:
+            formatted_split_fences = []
+            for fence in split_fences:
+                for group in fence:
+                    formatted_split_fences.append([p.snapshot()
+                                                   for p in group])
+                formatted_split_fences.append([])
+            stages.append(formatted_split_fences[:-1])
+            stagenames.append("Grouping prims of each boundary by "
+                              "target device")
+
+        ############## ALIGN SEQUENCES AND PAD FRAMES ##############
+
+        grouped_fences = [
+            FrameSequence(self._chain, *fence).finalize()
+            for f_i, fence in enumerate(split_fences)
+        ]
+
+        if debug:
+            formatted_grouped_fences = []
+            for fence in grouped_fences:
+                formatted_grouped_fences += fence.snapshot() + [[]]
+            stages.append(formatted_grouped_fences[:-1])
+            stagenames.append("Aligning and combining each group dev "
+                              "prim stream")
+
+        ################## RECOMBINE FRAME GROUPS ##################
+
+        ingested_chain = grouped_fences[0]
+        for fence in grouped_fences[1:]:
+            ingested_chain += fence
+
+        if debug:
+            stages.append(ingested_chain.snapshot())
+            stagenames.append("Recombining sanitized exec boundaries")
+
+        ###################### POST INGESTION ######################
+        ################ Flatten out LV3 Primitives ################
+        while(any((f._layer == 3 for f in ingested_chain))):
+            ################# COMBINE COMPATIBLE PRIMS #################
+            ingested_chain = mergePrims(self._chain, ingested_chain)
+
+            if debug:
+                stages.append(ingested_chain.snapshot())
+                stagenames.append("Combining compatible lv3 prims.")
+
+            ################ TRANSLATION TO LOWER LAYER ################
+            sm = JTAGStateMachine(self._chain._sm.state)
+            expanded_prims = FrameSequence(self._chain)
+            for f in ingested_chain:
+                if f._layer == 3:
+                    expanded_prims += f.expand_macro(sm)
+                else:
+                    expanded_prims.append(f)
+            expanded_prims.finalize()
+            ingested_chain = expanded_prims
+
+            if debug:
+                stages.append(ingested_chain.snapshot())
+                stagenames.append("Expanding lv3 prims")
+
+
+        ############## Flatten out Dev LV2 Primitives ##############
+        while(any((isinstance(f._valid_prim, DeviceTarget)
+                   for f in ingested_chain))):
+            ################# COMBINE COMPATIBLE PRIMS #################
+            ingested_chain = mergePrims(self._chain, ingested_chain)
+
+            if debug:
+                stages.append(ingested_chain.snapshot())
+                stagenames.append("Merging Device Specific Prims")
+
+            ################ TRANSLATION TO LOWER LAYER ################
+
+            sm = JTAGStateMachine(self._chain._sm.state)
+            expanded_prims = FrameSequence(self._chain)
+            for f in ingested_chain:
+                if issubclass(f._prim_type, DeviceTarget):
+                    expanded_prims += f.expand_macro(sm)
+                else:
+                    expanded_prims.append(f)
+            expanded_prims.finalize()
+            ingested_chain = expanded_prims
+
+            if debug:
+                stages.append(ingested_chain.snapshot())
+                stagenames.append("Expanding Device Specific Prims")
+
+        ############ Convert FrameSequence to flat array ###########
+        flattened_prims = [f._valid_prim for f in ingested_chain]
+        if debug:
+            stages.append([[p.snapshot() for p in flattened_prims]])
+            stagenames.append("Converting format to single stream.")
+
+        del ingested_chain
+
+        ######### Flatten out remaining macros Primitives #########
+        while (not all((isinstance(p, (ExpandRequiresTAP,Executable))
+                        for p in flattened_prims))):
+            ################# COMBINE COMPATIBLE PRIMS #################
+            flattened_prims = mergePrims(self._chain, flattened_prims)
+
+            if debug:
+                stages.append([[p.snapshot() for p in flattened_prims]])
+                stagenames.append("Merging Device Agnostic LV2 Prims")
+
+            ################ TRANSLATION TO LOWER LAYER ################
+            sm = JTAGStateMachine(self._chain._sm.state)
+            expanded_prims = []
+            for p in flattened_prims:
+                tmp = p.expand(self._chain, sm) if not \
+                      isinstance(p, ExpandRequiresTAP) else None
+                if tmp:
+                    expanded_prims += tmp
+                else:
+                    expanded_prims.append(p)
+            flattened_prims = expanded_prims
+
+            if debug:
+                stages.append([[p.snapshot() for p in flattened_prims]])
+                stagenames.append("Expanding Device Agnostic LV2 Prims")
+
+
+        ################# COMBINE COMPATIBLE PRIMS #################
+        flattened_prims = mergePrims(self._chain, flattened_prims)
+
+        if debug:
+            stages.append([[p.snapshot() for p in flattened_prims]])
+            stagenames.append("Final LV2 merge")
+
+        ################### EXPAND TO LV1 PRIMS ####################
+        sm = JTAGStateMachine(self._chain._sm.state)
+        expanded_prims = []
+        for p in flattened_prims:
+            tmp = p.expand(self._chain, sm)
+            if tmp:
+                expanded_prims += tmp
+            else:
+                expanded_prims.append(p)
+        flattened_prims = expanded_prims
+
+        if debug:
+            stages.append([[p.snapshot() for p in flattened_prims]])
+            stagenames.append("Expand to LV1 Primitives")
+
+
+        if not all((isinstance(p, Executable) for p in flattened_prims)):
+            raise ProteusISCError(
+                "Reduction did not produce executable instruction sequence.")
+
+
+        #################### COMBINE LV1 PRIMS #####################
+
+        flattened_prims = mergePrims(self._chain, flattened_prims)
+
+        if debug:
+            stages.append([[p.snapshot() for p in flattened_prims]])
+            stagenames.append("Final LV2 merge")
+
+        ############################################################
+
+        if not dryrun:
+            self.queue = flattened_prims
+
+
+def mergePrims(chain, inseq):
+    if isinstance(inseq, FrameSequence):
+        merged_prims = FrameSequence(chain)
+    else:
+        merged_prims = []
+    working_prim = inseq[0]
+    i = 1
+    while i < len(inseq):
+        tmp = inseq[i]
+        res = working_prim.merge(tmp)
+        if res is not None:
+            working_prim = res
+        else:
+            merged_prims.append(working_prim)
+            working_prim = tmp
+        i += 1
+    merged_prims.append(working_prim)
+    return merged_prims
