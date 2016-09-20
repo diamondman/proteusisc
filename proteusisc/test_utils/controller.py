a802e5bacbed86f324c4e3fa1ccf492889a130b0
Jessy Diamond Exum <jessy.diamondman@gmail.com>
2016-09-19 22:46:23 -0700

    Added Documentation to FakeDevHandle

--- proteusisc/test_utils/controller.py
+++ proteusisc/test_utils/controller.py
@@ -1,4 +1,5 @@
 from bitarray import bitarray
+# pylint: disable=no-name-in-module
 from usb1 import USBErrorPipe, USBErrorOverflow
 
 from ..drivers.digilentdriver import _CMSG_PROD_NAME, _CMSG_USER_NAME,\
@@ -6,9 +7,31 @@ from ..drivers.digilentdriver import _CMSG_PROD_NAME, _CMSG_USER_NAME,\
     _CMSG_PROD_ID, _CMSG_OEM_CHECK
 
 class FakeDevHandle(object):
+    """Artificial USB Digilent ISC controller that implements the usb1.USBDeviceHandle interface.
+
+    With real USB ISC Controllers, a USBDevice can be used to get a
+    Handle to th communicate with the physical device. But simulating
+    the full USB subsysem, or hooking into it, is too much of a pain,
+    so simulating a USB device is more easily done by just handling
+    the application level USB library functions directly.
+
+    FakeDevHandle is treated as a handle, but it is really the
+    simulated USB device.
+
+    Understanding the USB related functions of this class requires
+    understanding libusb1.0 and USB in general, most ththe types of
+    requests, what endpoints are, and what fields are required for
+    each message type. The following links should be enough to get
+    started:
+        http://www.beyondlogic.org/usbnutshell/usb6.shtml
+        https://github.com/vpelletier/python-libusb1/blob/master/usb1.py
+        http://libusb.sourceforge.net/api-1.0/group__syncio.html#gadb11f7a761bd12fc77a07f4568d56f38
+
+    """
     USB_VEND_ID = 0x1443
     USB_PROD_ID = 0x0007
 
+    # Special Controller specific constants.
     PROD_NAME = b'CoolRunner 2 Starter 2'
     USER_NAME = b'Cr2s2'
     SERIAL_NO = b"10146D508907"
@@ -16,6 +39,9 @@ class FakeDevHandle(object):
     PROD_ID = b'\x00\x90\x01\x26'#'0x00900126
     DEV_CAP = b'\x00\x00\x00\x15'#0x00000015
 
+    #Digilent controllers use a 4 byte bulk packet header to specify
+    #what command will be run. This structure is a mappint of
+    # (category, request) : (command_name, init_packet_length)
     BLK_HANDLERS = {
         (0x02, 0x00): ("ENABLE_JTAG", 0x03),
         (0x02, 0x01): ("DISABLE_JTAG", 0x03),
@@ -35,27 +61,71 @@ class FakeDevHandle(object):
         self.devices = devices
         self._jtag_on = False
         self._oem_seed = b'\x00\x00'
-        self._blk_read_buffer2 = []
+        self._blk_read_buffer = []
         self._lastcmd = None
         self._adv_req_enabled = False
 
     @property
     def jtagon(self):
+        """Check if the controller's JTAG enable bit is set"""
         return self._jtag_on
 
     def close(self):
+        """Close the handle.
+
+        This is required for real hardware, but for a simulation that
+        is really just a controller, nothing has to happen. The
+        function is required to comply with the usb1.USBDeviceHandle
+        interface."""
         pass
 
     def controlWrite(self, request_type, request, value, index, data,
                      timeout=0):
+
+        """Simulates libusb's synchronous control write in a sandbox.
+
+        Only features explicitly used by the simulated controller's
+        drier are implemented, so important USB management messages
+        are not implemented.
+
+        For details about this interface and arguments, refer to the
+        links provided in the class description.
+
+        For details on the commands supported by this simulated
+        controller, please consult the controller doncumentation:
+            http://diamondman.github.io/Adapt/cable_digilent_adept.html#control-messages
+
+        Returns:
+            An integer length of the number of bytes sent.
+
+        """
         if request_type & 0x80 is not 0x00:
             raise Exception("Incorrect data direction in reqtype")
 
         if request is _CMSG_OEM_SEED:
             self._oem_seed = data
 
+        return len(data)
+
     def controlRead(self, request_type, request, value, index, length,
                     timeout=0):
+        """Simulates libusb's synchronous control read in a sandbox.
+
+        Only features explicitly used by the simulated controller's
+        drier are implemented, so important USB management messages
+        are not implemented.
+
+        For details about this interface and arguments, refer to the
+        links provided in the class description.
+
+        For details on the commands supported by this simulated
+        controller, please consult the controller doncumentation:
+            http://diamondman.github.io/Adapt/cable_digilent_adept.html#control-messages
+
+        Returns:
+            A bytes buffer containing the data read from the controller.
+            #TODO Add details about format to docstring or documentation.
+        """
         if request_type & 0x80 is not 0x80:
             raise Exception("Incorrect data direction in reqtype")
 
@@ -83,6 +153,47 @@ class FakeDevHandle(object):
         return res
 
     def bulkWrite(self, endpoint, data, timeout=0):
+        """Simulates libusb's synchronous bulk write in a sandbox.
+
+        Almost all RPC calls to the Digilent JTAG controller are
+        started with a bulk write. The message contains a length, a
+        cateory, a command, and optional parameters. The format of the
+        initialization request is described at:
+            http://diamondman.github.io/Adapt/cable_digilent_adept.html#bulk-requests
+
+        This function has to update the state of the 'controller' so
+        further messages can finish the RPC request. Each command can
+        have a unique sequence of messages, so each command is handled
+        with its own function .
+
+        For example, if a message came in starting with
+            0x09 02 0B 00
+
+        The category is 2, and the command is 0x0B. In the table
+        'BLK_HANDLERS', (2, 0xB) maps to ("WRITE_TMS", 9) which is the
+        name and required length. The required length field matches,
+        so the function _handle_blk_WRITE_TMS will be called.
+
+        The WRITE_TMS handler will decode all the RPC specific
+        parameters, and prepare the simulated device for the next
+        write stage. A status message (2 bytes) will also be added to
+        the return buffer to be retrieved on the next read.
+
+        The next bulk write will be treated as the data stage of the
+        Digilent 'advanced transfer protocol' specified in the above
+        controller documentation link. This requrest will be handled
+        by the stage2 handler for WRITE_TMS:
+            _handle_blk_WRITE_TMS_stage2
+
+        If data was requested to be returned, it will be put on the
+        return buffer to be read later.
+
+        Finally, statistics on the last operation may be requested by
+        setting the 7th bit of Request to 1. These statistics will be
+        put on the return buffer and can be read out later.
+
+        """
+
         if not self._adv_req_enabled:
             if len(data) < 4:
                 raise Exception("Too short message. Min 4 bytes")
@@ -109,11 +220,11 @@ class FakeDevHandle(object):
                 handler = getattr(self, handler_name, None)
                 if not handler:
                     raise Exception("No handler %s. Would Stall."%
-                                    handler_name)
+                                    handler_name) # pragma: no cover
 
-                #print("BEFOREEXEC %s******"%name, self._blk_read_buffer2)
+                #print("BEFOREEXEC %s******"%name, self._blk_read_buffer)
                 handler(params)
-                #print("AFTER EXEC******", self._blk_read_buffer2)
+                #print("AFTER EXEC******", self._blk_read_buffer)
         else:
             #For Advanced messages, there is no header, just raw data.
             self._adv_req_enabled = False
@@ -125,26 +236,49 @@ class FakeDevHandle(object):
                           **self._adv_req_extra_params)
 
     def bulkRead(self, endpoint, length, timeout=0):
-        if self._blk_read_buffer2:
-            #print("BEFORE READ******", self._blk_read_buffer2)
-            res = self._blk_read_buffer2[0]
-            del self._blk_read_buffer2[0]
-            #print("AFTER READ******", self._blk_read_buffer2)
+        if self._blk_read_buffer:
+            #print("BEFORE READ******", self._blk_read_buffer)
+            res = self._blk_read_buffer[0]
+            del self._blk_read_buffer[0]
+            #print("AFTER READ******", self._blk_read_buffer)
             return res
         raise Exception("Would Hang waiting for something to return")
 
     def _report_advanced_metrics(self):
-        #Return metrics
+        """Report the metrics of the last RPC.
+
+        Digilent RPCs can be followed up with a call to check the
+        statistics of the last command. This includes the number of
+        bits writte, if a read happened, and the number of bits read
+        (only if a read did occur).
+
+        When called, this puts the metrics packet in the response
+        buffer for retrieval on a future read..
+
+        """
         sz = 1 + 4 + (4*bool(self._adv_req_read_tdo))
         flags = 0x80 + (0x40 if self._adv_req_read_tdo else 0)
         writecount = self._adv_req_bitcount.to_bytes(4, 'little')
         readcount = self._adv_req_bitcount.to_bytes(4, 'little')\
                     if self._adv_req_read_tdo else b''
-        self._blk_read_buffer2.append(
+        self._blk_read_buffer.append(
             b'%c%c'%(sz, flags) + writecount\
             + readcount)
 
     def _write_to_dev_chain(self, tms, tdi):
+        """Simulate electrically asserting a bit to the JTAG output pins.
+
+        Args:
+            tms: A boolean to be asserted to every simulated device's
+             (in the chain) tms bit.
+            tdi: A boolean to be shifted into the first simulated
+             device's tdi pin.
+
+        Returns:
+            The boolean read from the tdo pin of the last device in
+            the scan chain
+
+        """
         #oldstate = self.devices[0].tap.state
         for dev in self.devices:
             tdi = dev.shift(tms, tdi)
@@ -154,6 +288,7 @@ class FakeDevHandle(object):
 
     def _initialize_advanced_return(self, bitcount, read_tdo,
                                     **params):
+        """Helper method to assign some important values to keep track of Digilent controller state."""
         self._adv_req_enabled = True
         self._adv_req_bitcount = bitcount
         self._adv_req_read_tdo = read_tdo
@@ -161,20 +296,20 @@ class FakeDevHandle(object):
 
     def _handle_blk_ENABLE_JTAG(self, params):
         if self._jtag_on:
-            self._blk_read_buffer2.append(b'\x01\x03')
+            self._blk_read_buffer.append(b'\x01\x03')
         else:
             self._jtag_on = True
-            self._blk_read_buffer2.append(b'\x01\x00')
+            self._blk_read_buffer.append(b'\x01\x00')
 
     def _handle_blk_DISABLE_JTAG(self, params):
         self._jtag_on = False
-        self._blk_read_buffer2.append(b'\x01\x00')
+        self._blk_read_buffer.append(b'\x01\x00')
 
     def _handle_blk_WRITE_TMS_TDI(self, params):
         doreturn = bool(params[0])
         bitcount = sum([b<<(i*8) for i,b in enumerate(params[1:5])])
         self._initialize_advanced_return(bitcount, doreturn)
-        self._blk_read_buffer2.append(b'\x01\x00')
+        self._blk_read_buffer.append(b'\x01\x00')
     def _handle_blk_WRITE_TMS_TDI_stage2(self, data, bitcount, read_tdo):
         bits = bitarray()
         bits.frombytes(data[::-1])
@@ -187,14 +322,14 @@ class FakeDevHandle(object):
         if read_tdo:
             tdo_bits = bitarray(([False]*(8-(len(tdo)%8)))+tdo[::-1])
             tdo_bytes = tdo_bits.tobytes()
-            self._blk_read_buffer2.append(tdo_bytes[::-1])
+            self._blk_read_buffer.append(tdo_bytes[::-1])
 
     def _handle_blk_WRITE_TMS(self, params):
         doreturn = bool(params[0])
         tdi = params[1]
         bitcount = sum([b<<(i*8) for i,b in enumerate(params[2:6])])
         self._initialize_advanced_return(bitcount, doreturn, tdi=tdi)
-        self._blk_read_buffer2.append(b'\x01\x00')
+        self._blk_read_buffer.append(b'\x01\x00')
     def _handle_blk_WRITE_TMS_stage2(self, data, bitcount,
                                      read_tdo, *, tdi):
         bits = bitarray()
@@ -206,7 +341,7 @@ class FakeDevHandle(object):
         if read_tdo:
             tdo_bits = bitarray(([False]*(8-(len(tdo)%8)))+tdo[::-1])
             tdo_bytes = tdo_bits.tobytes()
-            self._blk_read_buffer2.append(tdo_bytes[::-1])
+            self._blk_read_buffer.append(tdo_bytes[::-1])
 
     def _handle_blk_READ_TDO(self, params):
         tms = params[0]
@@ -218,17 +353,17 @@ class FakeDevHandle(object):
 
         self._adv_req_bitcount = bitcount
         self._adv_req_read_tdo = True
-        self._blk_read_buffer2.append(b'\x01\x00')
+        self._blk_read_buffer.append(b'\x01\x00')
         tdo_bits = bitarray(([False]*(8-(len(tdo)%8)))+tdo[::-1])
         tdo_bytes = tdo_bits.tobytes()
-        self._blk_read_buffer2.append(tdo_bytes[::-1])
+        self._blk_read_buffer.append(tdo_bytes[::-1])
 
     def _handle_blk_WRITE_TDI(self, params):
         doreturn = bool(params[0])
         tms = params[1]
         bitcount = sum([b<<(i*8) for i,b in enumerate(params[2:6])])
         self._initialize_advanced_return(bitcount, doreturn, tms=tms)
-        self._blk_read_buffer2.append(b'\x01\x00')
+        self._blk_read_buffer.append(b'\x01\x00')
     def _handle_blk_WRITE_TDI_stage2(self, data, bitcount,
                                      read_tdo, *, tms):
         bits = bitarray()
@@ -241,9 +376,34 @@ class FakeDevHandle(object):
         if read_tdo:
             tdo_bits = bitarray(([False]*(8-(len(tdo)%8)))+tdo[::-1])
             tdo_bytes = tdo_bits.tobytes()
-            self._blk_read_buffer2.append(tdo_bytes[::-1])
+            self._blk_read_buffer.append(tdo_bytes[::-1])
 
 class FakeUSBDev(object):
+    """The most basic features required for simulating a usb1.USBDevice.
+
+    Normally, a usb1.USBDevice is retrieved with:
+        >>> import usb1
+        >>> context = usb1.USBContext()
+        >>> dev = context.getDeviceList()[0]
+
+    The USBDevice only is used to check ids to match a driver, and
+    open a handle (type usb1.USBDeviceHandle) that can be used to talk
+    to the physical device.  The handle is retrieved from the
+    USBDevice with the open method:
+        >>> handle = dev.open()
+        >>> handle.controlWrite(........) #DO USB THINGS
+
+    A real USBDevice must track many states and pointers to usb
+    resources, including managing open handles. A fake USBDevice only
+    has to provide a handle when open is called, and report the
+    appropriate vendorID and productID.
+
+    Attributes:
+        ctrl_handle: An implementation of the usb1.USBDeviceHandle
+        interface that will be returned when the FakeUSBDev's open()
+        method is called.
+
+    """
     def __init__(self, mockPhysicalController):
         self.ctrl_handle = mockPhysicalController
     def open(self):
